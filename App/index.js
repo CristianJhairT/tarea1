1033dc8f370a21001116a16b285530494b6ce5e4
CristianJhairT <cristianjhair2000@gmail.com>
2021-05-24 20:04:26 -0500

    abc

--- App/index.js
+++ App/index.js
@@ -20,17 +20,16 @@ const scrapingProfile = () => {
     });
   };
 
+  //EDUCACIÓN
+  const elementNameProfile = document.querySelector("#ember44 > div.ph5.pb5 > div.display-flex.justify-space-between.pt2 > div > div:nth-child(1) > h1");
+  const elementNameUniversity = document.querySelector("#ember124 > div.pv-entity__summary-info.pv-entity__summary-info--background-section > div > h4.pv-entity__date-range.t-14.t-black--light.t-normal > span:nth-child(2)");
+  const elementNameTitle = document.querySelector("#ember131 > div.pv-entity__summary-info.pv-entity__summary-info--background-section > div > h3");
 
-  const elementNameProfile = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ul li")
-  const elementNameTitle = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 h2")
+ // const elementNameTitle = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 h2")
   const name = elementNameProfile ? elementNameProfile.innerText : '';
-  const title = elementNameTitle ? elementNameTitle.innerText : '';
-  wait(2000)
-  const elementMoreResume = document.getElementById('line-clamp-show-more-button')
-  if (elementMoreResume) elementMoreResume.click();
-  const elementResume = document.querySelector('section.pv-about-section > p')
-  const resume = elementResume ? elementNameTitle.innerText : '';
+  const univ = elementNameUniversity ? elementNameUniversity : '';
+ const title = elementNameTitle ? elementNameTitle.innerText : '';
 
 
-  console.log({ name, title, resume })
+  console.log({ name, univ, title})
 }
